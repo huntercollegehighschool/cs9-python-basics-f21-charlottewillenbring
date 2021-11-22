90813e0e984d65e532c88847f94cc8c208948e30
Charlotte Willenbring <>
2021-11-22 00:34:38 +0000

    Parts 1, 2, 3, 4, and 5, which are now working code

--- part2.py
+++ part2.py
@@ -11,5 +11,8 @@ number in the formatted sentence that's already there.
 
 #code starts here
 number = input("Enter a number: ")
-print("Ten times your number is", 10 * number)
-print("Ninety-nine more than your number is", 99 + number)
+number = int(number)
+print("Ten times your number is " + str(10*number))
+print("Ninety-nine more than your number is " + str(99 + number))
+
+
